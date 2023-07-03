c35bead11735ba3451d123a6df3b105d33da6098
lucas <lucasriibeiro23@gmail.com>
2023-07-03 16:28:07 -0300

    implementei props e renderizacao condicional ao projeto

---
+++ src/components/book.tsx
@@ -0,0 +1,26 @@
+type BookProps = {
+  book: {
+    title: string,
+    pages: number,
+    isRead: boolean,
+    isFavorite: boolean,
+  }
+};
+
+function Book({ book }: BookProps) {
+  return (
+    <li>
+      <span>
+        {book.isRead ? '✅' : '⌛'}
+        {' '}
+      </span>
+      <span>
+        {`${book.title} (${book.pages} paginas)`}
+      </span>
+      {' '}
+      {book.isFavorite && <span>🤩</span>}
+    </li>
+  );
+}
+
+export default Book;
