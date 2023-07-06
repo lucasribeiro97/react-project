d89617cb9807a9784fec617ea586fc06886fafe4
lucas <lucasriibeiro23@gmail.com>
2023-07-06 15:21:42 -0300

    trabalhando com event handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import Header from './components/header';
 import Footer from './components/footer';
+import Button from './components/button';
 
 import './App.css';
 
 function App() {
-  function handleClick() {
-    alert('Botao clicado!');
+  function handleButton1() {
+    alert('Botao 1 clicado!');
+  }
+  function handleButton2() {
+    alert('Botao 2 clicado!');
   }
   return (
     <div className="app">
       <Header />
       <div className="container">
-        <button onClick={ handleClick }>
-          Clique aqui
-        </button>
+        <Button
+          onClick={ handleButton1 }
+        >
+          Botao 1
+        </Button>
+
+        <Button
+          onClick={ handleButton2 }
+        >
+          Botao 2
+        </Button>
       </div>
       <Footer />
     </div>
---
+++ src/components/button/index.tsx
@@ -0,0 +1,18 @@
+import './button.css';
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick: () => void;
+};
+
+function Button({ children, onClick }: ButtonProps) {
+  return (
+    <button
+      onClick={ onClick }
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
