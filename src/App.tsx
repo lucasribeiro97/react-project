3449fc085a13483f9b26944c8615bc284feaae1b
lucas <lucasriibeiro23@gmail.com>
2023-07-06 14:55:27 -0300

    aprendendo eventos em react

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,21 @@
 import Header from './components/header';
 import Footer from './components/footer';
-import Book from './components/book';
-import Title from './components/title';
 
 import './App.css';
 
-import { booksList, bestMovie } from './data';
-
-import interestelar from './assets/interestelar.jpeg';
-
-import { AiOutlineMail } from 'react-icons/ai';
-
 function App() {
+  function handleClick() {
+    alert('Botao clicado!');
+  }
+
   return (
     <div className="app">
       <Header />
       <div className="container">
-        <Title>
-          <s>Livros emprestados</s>
-          Meus Livros:
-        </Title>
-        <ul className="books-list">
-          {booksList.map((book) => (
-            <Book
-              key={ book.title }
-              book={ book }
-            />
-          ))}
-        </ul>
-
-        <Title>
-          {bestMovie}
-        </Title>
-
-        <div className="images-container">
-          <img src={ interestelar } alt="#" />
-          <img src="/interstellar-public.jpg" alt="#" />
-        </div>
+        <button onClick={ handleClick }>
+          Clique aqui
+        </button>
       </div>
-      <AiOutlineMail />
       <Footer />
     </div>
   );
